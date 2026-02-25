2a7dbb50d24a2711e7603a67594316dff20e3292
jguerreiro <joel.borghi@gmail.com>
2026-02-25 12:18:22 -0300

    Feat: Open/close modal feature. - I was able to lift the state and pass it to children components. - That way I was able to change the state from children component. - When clicking the close button it successfully closes the modal.

--- src/Components/Footer/index.tsx
+++ src/Components/Footer/index.tsx
@@ -1,10 +1,10 @@
 import { StyledFooter } from './style';
 
-interface FooterProps {
+interface footerProps {
     children: React.ReactNode;
 }
 
-export const Footer = ({ children }: FooterProps) => {
+export const Footer = ({ children }: footerProps) => {
     return (
         <StyledFooter>
             {children}
